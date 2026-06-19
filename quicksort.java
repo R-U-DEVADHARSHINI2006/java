4835c9b6200e640d1eac295cf84fed51a76a2eb6
Devadharshini R U <155797442+R-U-DEVADHARSHINI2006@users.noreply.github.com>
2026-06-19 20:34:18 +0530

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -1,48 +1,43 @@
 package itjava.example;
+import java.util.Arrays;
+public class quicksort1 {
+  public static int partition(int[] arr, int left, int right) {
+      int pivot = arr[left];
+      int i = left;
+      int j = right;
+      while (i < j) {
+          while (i <= right && arr[i] <= pivot) {
+              i++;
+          }
+          while (j >= left && arr[j] > pivot) {
+              j--;
+          }
+          if (i < j) {
+              swap(arr, i, j);
+          }
+      }
+      swap(arr, j, left);
+      //System.out.println(Arrays.toString(arr));
+      return j;
+  }
 
-public class QuickSort {
-	 public static void quickSort(int[] arr, int left, int right) {
+  public static void swap(int[] arr, int i, int j) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+  }
 
-	        if (left < right) {
+  public static void quickSort(int[] arr, int left, int right) {
+      if (left < right) {
+          int pivot = partition(arr, left, right);
+          quickSort(arr, left, pivot - 1);
+          quickSort(arr, pivot + 1, right);
+      }
+  }
 
-	            int pivotIndex = partition(arr, left, right);
-
-	            quickSort(arr, left, pivotIndex - 1);
-	            quickSort(arr, pivotIndex + 1, right);
-	        }
-	    }
-
-	    public static int partition(int[] arr, int left, int right) {
-
-	        int pivot = arr[right];
-	        int i = left - 1;
-
-	        for (int j = left; j < right; j++) {
-
-	            if (arr[j] < pivot) {
-	                i++;
-
-	                int temp = arr[i];
-	                arr[i] = arr[j];
-	                arr[j] = temp;
-	            }
-	        }
-
-	        int temp = arr[i + 1];
-	        arr[i + 1] = arr[right];
-	        arr[right] = temp;
-
-	        return i + 1;
-	    }
-
-	    public static void main(String[] args) {
-
-	        int[] arr = {10, 7, 8, 9, 1, 5};
-
-	        quickSort(arr, 0, arr.length - 1);
-
-	        for (int num : arr) {
-	            System.out.print(num + " ");
-	        }
-	    }
+  public static void main(String[] args) {
+      int[] arr = {5, 12, 4, 9, 2, 7, 1, 10};
+      quickSort(arr, 0, arr.length - 1);
+      System.out.println(Arrays.toString(arr));
+  }
 }
